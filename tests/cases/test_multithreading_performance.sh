41ffb7efa16a3ff2cefb3a71ca5eb0b81e831811
Petya <petrulit3@mail.ru>
2025-12-13 18:18:40 +0300

    fix: fixed number of iterations

--- tests/cases/test_multithreading_performance.sh
+++ tests/cases/test_multithreading_performance.sh
@@ -10,7 +10,7 @@ measure_time() {
 
     # Измерение времени выполнения
     START_TIME=$(date +%s)
-    "$EXECUTABLE_PATH" -w 1920 -h 1080 -t "$threads" -o "$output_file"
+    "$EXECUTABLE_PATH" -w 1920 -h 1080 -t "$threads" -o "$output_file" -i 50000000 --seed 71 -s 4
     EXIT_CODE=$?
     END_TIME=$(date +%s)
 
