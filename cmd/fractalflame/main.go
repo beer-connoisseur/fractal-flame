61a9b8c3f050d6e7d99ad667d7b94efdb62e7ee6
Petya <petrulit3@mail.ru>
2025-12-13 18:16:45 +0300

    feat: added entrypoint

--- cmd/fractalflame/main.go
+++ cmd/fractalflame/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/application"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/cli"
+)
+
 func main() {
-	// TODO: write your code here
+	app := cli.NewApp()
+
+	err := app.Run(os.Args)
+	if err != nil {
+		slog.Error("Application failed", "error", err)
+	}
+	exitCode := application.GetExitCode(err)
+
+	os.Exit(exitCode)
 }
